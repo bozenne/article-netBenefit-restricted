e29d7dba33da4e472f78ddcbcacb888e06b9ab99
Brice Maxime Hugues Ozenne <hpl802@ku.dk>
2024-02-08 13:49:52 +0100

    update simulation results crossing

--- BATCH_scenario4-Crossing.R
+++ BATCH_scenario4-Crossing.R
@@ -125,9 +125,6 @@ for(iSim in 1:n.sim){ ## iSim <- 1
         n.Control <- 200
         n <- n.Treatment+n.Control
 
-      	HR1C <- 0.15
-        HR1T <- 0.2
-
         HazC <- 0.10
         HazT <- 0.18
 
